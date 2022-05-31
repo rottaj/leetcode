683c660ee4c6dab7ddeb11307ddbae959a0c6f7d
rottaj <jackrotta12@gmail.com>
2022-05-31 13:55:24 -0400

    Updated

--- python/102-Binary-Tree-Level-Order-Traversal.py
+++ python/102-Binary-Tree-Level-Order-Traversal.py
@@ -6,55 +6,35 @@ class TreeNode:
 
 
 class Solution:
-
-  def traverse(self, node, arr, direction):
-    arr.append(node.val)
-    if direction == "right":
-      if node.right != None:
-        return self.traverse(node.right, arr, 'right')
-      else:
-        return arr
-    elif direction == "left":
-      if node.left != None:
-        return self.traverse(node.left, arr, 'left')
-      else:
-        return arr       
-
-
-  def build(self, left, right):
-    ret = [left[0]]
-    length = 0
-    if len(left) > len(right):
-      length = len(left)
-    else:
-      length = len(right)
-    print(length)
-    while len(ret) <= length:
-      try:
-        temp = [left[len(ret)], right[len(ret)]]
-      except: None
-      ret.append(temp)
-      print("RET", ret)
-
   def levelOrder(self, root):
-    branchRight = self.traverse(root, [], 'right')
-    branchLeft = self.traverse(root, [], 'left')
-    print("LEFT", branchLeft)
-    print("RIGHT", branchRight)
-    self.build(branchLeft, branchRight)
+    res, curr = [], [root]
+    if root is None:
+      return []
+    else:
+      res.append(root.val)
+    while curr:
+      temp = []
+      for node in curr:
+        if node.left:
+          temp.append(node.left) 
+        if node.right:
+          temp.append(node.right)
+      if len(temp) is not 0:
+        res.append([f.val for f in temp])
+      curr = temp 
+    return res
 
+if __name__ == '__main__':
 
+  root = TreeNode(3)
+  rootLeft = TreeNode(9)
+  rootRight = TreeNode(20)
+  branchRightChildRight = TreeNode(7)
+  branchRightChildLeft = TreeNode(15)
+  rootRight.left = branchRightChildLeft
+  rootRight.right = branchRightChildRight 
+  root.left = rootLeft
+  root.right = rootRight
 
-root = TreeNode(3)
-rootLeft = TreeNode(9)
-rootRight = TreeNode(20)
-branchRightChildRight = TreeNode(7)
-branchRightChildLeft = TreeNode(15)
-rootRight.left = branchRightChildLeft
-rootRight.right = branchRightChildRight 
-root.left = rootLeft
-root.right = rootRight
-
-solve = Solution()
-
-solve.levelOrder(root)
+  ans = Solution().levelOrder(root)
+  print("Solve", ans)
