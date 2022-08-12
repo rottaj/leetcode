1c91cbd7e575ba4d1807142439af599884829e88
rottaj <jackrotta12@gmail.com>
2022-08-12 19:18:02 -0400

    Updated

---
+++ google/11_Container_With_Most_Water.py
@@ -0,0 +1,10 @@
+class Solution:
+  def maxArea(self, height):
+    print(height)
+
+
+if __name__ == "__main__":
+  height = [1,8,6,2,5,4,8,3,7]
+  Solution().maxArea(height)
+  
+
---
+++ google/14_Longest_Common_Prefix.py
@@ -0,0 +1,6 @@
+class Solution:
+  def longestCommonPrefix(self, ):
+
+
+if __name__ == "__main__":
+  Solution().longestCommonPrefix()
